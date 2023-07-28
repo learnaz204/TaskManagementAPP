bf687b8fbc1319542704dadb2a615409d2ea9105
learnaz204 <127105008+learnaz204@users.noreply.github.com>
2023-07-28 17:52:57 +0530

    Add files via upload

---
+++ src/app/task-notes/task-notes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-notes',
+  templateUrl: './task-notes.component.html',
+  styleUrls: ['./task-notes.component.css']
+})
+export class TaskNotesComponent {
+
+}
