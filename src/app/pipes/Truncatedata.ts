03dbf0f1c32475ca7e683b47305b5390488f6f5a
learnaz204 <127105008+learnaz204@users.noreply.github.com>
2023-07-28 17:50:45 +0530

    Add files via upload

--- src/app/pipes/Truncatedata.ts
+++ src/app/pipes/Truncatedata.ts
@@ -1,1 +1,15 @@
+import { Pipe, PipeTransform } from "@angular/core";
 
+@Pipe({
+    name:'truncate',
+})
+
+export class Trucate implements PipeTransform{
+    transform(text: string):string {
+        if (text != null && text != undefined && text != '')
+        {
+            return text.trim();
+        }
+        return text;
+    }
+}
